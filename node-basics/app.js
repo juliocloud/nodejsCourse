b8602dcded25a5299bd0bf04548eaec32a3c6368
juliocjuri <juliocesarjuriolli@gmail.com>
2023-03-01 14:55:17 -0300

    first server creation

---
+++ node-basics/app.js
@@ -0,0 +1,21 @@
+/*
+* Requiring from a global package (not local)
+*/
+const http = require('http') 
+
+/*
+* As long as there are listeners registered, the event loop will keep executing
+*/
+function rqListener(req, res){
+    console.log(req)
+    process.exit() 
+    // We generally don't call an exit action. In this case, we're finishing the server imeddiatly when there's a requisition
+}
+
+const server = http.createServer(rqListener) //Request Listener as an argument (function that will execute for every request)
+
+/*
+* Listening to the server in the port 3000. Whenever I try to access localhost on port 3000
+* it will return to me the console log of the requisition
+*/
+server.listen(3000);
