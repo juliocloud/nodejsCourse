ec6970092ac7c8c5d943fe8be5edb61350b1c567
juliocjuri <juliocesarjuriolli@gmail.com>
2023-03-10 11:55:20 -0300

    form post method

--- node-basics/req-res.js
+++ node-basics/req-res.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs')
 
 /*
 * Here we are sending a response to the request: setting a header and
@@ -6,11 +7,24 @@ const http = require('http');
 */
 const server = http.createServer((req, res) => {
     const url = req.url
+    const method = req.method
+
+    /*
+    * We have a form with a POST method pointing /message address. And then
+    * we return the 302 status code
+    */
     if(url === '/'){
         res.write('<html>')
         res.write('<head> <title> Enter Message </title> </head>');
         res.write('<body> <form action="/message" method="POST"> <input type="text" name="message"></input><button type="submit"> Send </button> </form> </body>')
         res.write('</html>')
+        return res.end()
+    }
+
+    if(url === '/message' && method === 'POST'){
+        fs.writeFileSync("textoexemplo.txt", "Texto escrito com sucesso")
+        res.statusCode = 302
+        res.setHeader('Location', '/');
         return res.end()
     }
     res.setHeader('Content-Type', 'text/html');
