f379d4018deae6ef82d6ef9269edbb9bf3070041
juliocjuri <juliocesarjuriolli@gmail.com>
2023-03-10 11:16:56 -0300

    url conditional request

--- node-basics/req-res.js
+++ node-basics/req-res.js
@@ -1,12 +1,24 @@
 const http = require('http');
 
-
+/*
+* Here we are sending a response to the request: setting a header and
+* sending an html
+*/
 const server = http.createServer((req, res) => {
-    console.log(req.url);
+    const url = req.url
+    if(url === '/'){
+        res.write('<html>')
+        res.write('<head> <title> Enter Message </title> </head>');
+        res.write('<body> <form action="/message" method="POST"> <input type="text" name="message"></input><button type="submit"> Send </button> </form> </body>')
+        res.write('</html>')
+        return res.end()
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
-    res.write('<h1> Hello World </h1>');
-    res.write('</html>');
+    res.write('<head><title>My page</title></head>')
+    res.write('<body><h1> Hello </h1></body>')
+    res.write('</html>')
+    res.end()
 })
 
 server.listen(3000)
