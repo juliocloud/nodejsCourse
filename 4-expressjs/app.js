8ce1f441b591e4daaa264d231e28020cabe217a9
juliocjuri <juliocesarjuriolli@gmail.com>
2023-03-20 15:02:36 -0300

    folder configurations

--- 4-expressjs/app.js
+++ 4-expressjs/app.js
@@ -3,9 +3,22 @@ const http = require('http');
 
 const app = express();
 
+/* 
+* 
+* With expressjs, we usually work with middlewares, which are all functions placed 
+* between an http request and the response from the server to the client
+* 
+*/
+
 app.use((req, res, next) => {
-    console.log("Inside the middleware")
+    console.log("Inside the middleware");
+    next(); //When we start a middleware, we have to call the next() function in order to continue the execution of the other mws
 })
+
+app.use((req, res, next) => {
+    console.log("Next middleware");
+    res.send("<html> <h1> Express JS </h1> </html>")
+});
 
 const server = http.createServer(app)
 
