f133d11ea92270380a0b7f842bfce615d89524c8
juliocjuri <juliocesarjuriolli@gmail.com>
2023-04-10 14:31:13 -0300

    adjusts

--- 4-expressjs/app.js
+++ 4-expressjs/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
 
@@ -9,13 +10,19 @@ const app = express();
 * 
 */
 
-app.use((req, res, next) => {
-    console.log("Inside the middleware");
-    next(); //When we start a middleware, we have to call the next() function in order to continue the execution of the other mws
+app.use(bodyParser.urlencoded());
+
+app.use('/add-product',(req, res, next) => {
+    res.send("<form action='/product' method='POST'> <input type='text' name='title'> <button type='submit'> Add product </button> </input> </form>")
+ //When we start a middleware, we have to call the next() function in order to continue the execution of the other mws
 })
 
-app.use((req, res, next) => {
-    console.log("Next middleware");
+app.use('/product',(req, res, next) => {
+    console.log(req.body)
+    res.redirect('/')
+});
+
+app.use('/',(req, res, next) => {
     res.send("<html> <h1> Express JS </h1> </html>")
 });
 
---
+++ exercises/2/readme.txt
@@ -0,0 +1,10 @@
+Part 1: 
+    - Create a npm project and install Express.js (Nodemon if you want)
+
+Part 2: 
+    - Create express app which funnels requests through 2 middleware functions.
+
+Part 3:
+    - Handle requests to "/" and "/users" 
+    - Each request only has one handler
+    
