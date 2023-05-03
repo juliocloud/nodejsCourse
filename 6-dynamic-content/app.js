440315c091b8a89ed92209fea09aa438a23fcd7e
juliocjuri <juliocesarjuriolli@gmail.com>
2023-05-03 17:30:29 -0300

    storing user data

--- 6-dynamic-content/app.js
+++ 6-dynamic-content/app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const port = 3000;
@@ -17,7 +17,7 @@ const portCallback = () => {
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
